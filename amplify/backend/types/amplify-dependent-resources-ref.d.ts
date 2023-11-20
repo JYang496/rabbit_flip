a4b4cf94cd1aec45cff7799ec7605ecce15d018b
Jimmy Yang <128646419+JYang496@users.noreply.github.com>
2023-11-20 14:50:13 -0500

    Added graphql and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rabbitflip": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "rabbitflip1c43501d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3928d5ecb": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
